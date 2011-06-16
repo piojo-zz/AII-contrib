aaa62acf6c37237ee7366db695ec4667cb26b6de
stdweird <stdweird@b8b08b4b-6333-482b-9f54-fdf6c5fb3d79>
2011-06-16 12:46:59 +0000

    small fixes
    git-svn-id: https://quattor.svn.sourceforge.net/svnroot/quattor/trunk/aii@2799 b8b08b4b-6333-482b-9f54-fdf6c5fb3d79

--- hooks/aii-sindes/config.mk
+++ hooks/aii-sindes/config.mk
@@ -4,6 +4,6 @@ AUTHOR=Stijn De Weirdt
 MAINTAINER=Stijn De Weirdt
 DESCRIPTION=AII plug-in that generates Kickstart files.
 DESCR=$(DESCRIPTION)
-VERSION=0.3.0
+VERSION=0.3.1
 RELEASE=1
 DATE=24/03/08 17:04
